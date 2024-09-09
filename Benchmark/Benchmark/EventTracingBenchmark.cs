6569b20c36258df3ed6361d1572c5efaa6aa9d07
MatthewRawson <mrawson@redev.co.uk>
2024-09-09 09:38:38 +0100

    Added EventTracingBenchmark

---
+++ Benchmark/Benchmark/EventTracingBenchmark.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace Benchmark;
+
+[EtwProfiler]
+[MemoryDiagnoser]
+public class EventTracingBenchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public void BenchmarkFunction()
+    {
+        CodeToBenchmark();
+    }
+
+    private void CodeToBenchmark()
+    {
+    }
+}
+
