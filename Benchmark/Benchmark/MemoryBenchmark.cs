8a8675f27109e54cfa73fa937f5e8574dfeb224e
MatthewRawson <mrawson@redev.co.uk>
2024-09-09 09:38:45 +0100

    Added Memory Benchmark

---
+++ Benchmark/Benchmark/MemoryBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark;
+
+[MemoryDiagnoser]
+public class MemoryBenchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public void BenchmarkFunction()
+    {
+        CodeToBenchmark();
+    }
+
+    private void CodeToBenchmark()
+    {
+    }
+}
