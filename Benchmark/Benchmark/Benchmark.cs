27bfe4858740e0974be5b112e483003d1d4ec18a
MatthewRawson <mrawson@redev.co.uk>
2024-09-09 09:38:30 +0100

    Added Benchmark.cs

---
+++ Benchmark/Benchmark/Benchmark.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark;
+
+public class Benchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public void BenchmarkFunction() 
+    {
+        CodeToBenchmark();
+    }
+
+    private void CodeToBenchmark() 
+    {
+    }
+}
