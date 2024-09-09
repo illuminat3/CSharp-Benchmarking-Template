c76d15bd358ccb8fe033599017ead6e011143b5b
MatthewRawson <mrawson@redev.co.uk>
2024-09-09 09:38:51 +0100

    Added Quick Benchmark

---
+++ Benchmark/Benchmark/QuickBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark;
+
+[ShortRunJob]
+public class QuickBenchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark]
+    public void BenchmarkFunction()
+    {
+        CodeToBenchmark();
+    }
+
+    private void CodeToBenchmark()
+    {
+    }
+}
