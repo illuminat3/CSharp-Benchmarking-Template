7a50518aea8e40452767402d315487998ecf24e3
MatthewRawson <mrawson@redev.co.uk>
2024-09-09 09:38:14 +0100

    Added Program.cs

--- Benchmark/Benchmark/Program.cs
+++ Benchmark/Benchmark/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Benchmark>(); //Change <Benchmark> to the class containing your benchmarks
+        Console.WriteLine("Benchmarks Completed, press Enter to exit.");
+        Console.ReadLine();
+    }
+}
